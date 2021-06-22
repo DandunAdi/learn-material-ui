45442e053928ab62bb9a7e8639d3e39066ca1798
Dandun Adi <dandun.adi@gmail.com>
2021-06-22 15:23:20 +0700

    LEARN: Textfield component

--- src/pages/Create.js
+++ src/pages/Create.js
@@ -1,7 +1,43 @@
-import { Button, Container, Typography } from "@material-ui/core";
+import {
+  Button,
+  Container,
+  makeStyles,
+  TextField,
+  Typography,
+} from "@material-ui/core";
 import KeyboardArrowRightIcon from "@material-ui/icons/KeyboardArrowRight";
+import { useState } from "react";
+
+const useStyles = makeStyles({
+  fields: {
+    margin: "20px 0",
+  },
+});
 
 const Create = () => {
+  const classes = useStyles();
+  const [title, setTitle] = useState("");
+  const [details, setDetails] = useState("");
+  const [titleError, setTitleError] = useState(false);
+  const [detailsError, setDetailsError] = useState(false);
+
+  const handleSubmit = (e) => {
+    e.preventDefault();
+    setTitleError(false);
+    setDetailsError(false);
+
+    if (!title) {
+      setTitleError(true);
+    }
+    if (!details) {
+      setDetailsError(true);
+    }
+
+    if (title && details) {
+      console.log(title, details);
+    }
+  };
+
   return (
     <Container>
       <Typography
@@ -12,16 +48,40 @@ const Create = () => {
       >
         Create a New Note
       </Typography>
-      <Button
-        type="submit"
-        variant="contained"
-        color="primary"
-        disableElevation
-        onClick={() => console.log("clicked!")}
-        endIcon={<KeyboardArrowRightIcon />}
-      >
-        Submit
-      </Button>
+
+      <form noValidate autoComplete="off" onSubmit={handleSubmit}>
+        <TextField
+          className={classes.fields}
+          label="Note Title"
+          variant="outlined"
+          fullWidth
+          required
+          onChange={(e) => setTitle(e.target.value)}
+          error={titleError}
+        />
+
+        <TextField
+          className={classes.fields}
+          label="Details"
+          variant="outlined"
+          fullWidth
+          required
+          multiline
+          rows={4}
+          onChange={(e) => setDetails(e.target.value)}
+          error={detailsError}
+        />
+
+        <Button
+          type="submit"
+          variant="contained"
+          color="primary"
+          disableElevation
+          endIcon={<KeyboardArrowRightIcon />}
+        >
+          Submit
+        </Button>
+      </form>
     </Container>
   );
 };
